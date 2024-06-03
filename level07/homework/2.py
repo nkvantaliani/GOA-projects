39ada934f3629fa7e6f71bf35c248f94bedc91b8
mariamikv <mariamikv71@gmail.com>
2024-06-04 01:57:20 +0400

    level 07 homework

---
+++ level07/homework/2.py
@@ -0,0 +1,10 @@
+number = int(input('Your fav number:'))
+Number = int(input("Your angel number:"))
+NUMBER = int(input("The day you were born on:"))
+
+
+print(number + Number + NUMBER)
+print(number - Number - NUMBER)
+print(number * Number * NUMBER)
+print(number / Number / NUMBER)
+print(number // Number // NUMBER)

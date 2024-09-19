795ebeab6821d978799b8c7be354477514cdff8c
nkvantaliani <nataliakvantaliani2009@gmail.com>
2024-09-20 03:43:25 +0400

    commit

---
+++ level 57/classwork/index.js
@@ -0,0 +1,152 @@
+// 1
+
+// let PI = 3.14;
+// PI = 4;
+// console.log(PI);
+
+// const PI = 3.14;
+// PI = 7;
+// console.log(PI);
+
+// ჩვენ არ შეგვიძლია შევცვალოთ ცვლადი რომლის შემოტანის დროსაც გამოვიყენეთ const keyword
+
+// 2
+
+// let age = 15;
+// age = 84;
+
+// 3
+
+// let x = 12;
+// let y = 23;
+// x = 23;
+// y = 12;
+// console.log(y);
+
+// 4 
+
+// const favoriteColor = "Pink";
+// favoriteColor = "black";
+
+// 5 
+
+// let a = 10;
+// let b = 3;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// 6
+
+// let a = 0;
+// a = 2 ** 5;
+// console.log(a);
+
+// 7
+
+// let width = 5;
+// let height = 10;
+// let area = (width + height) * 2;
+// console.log(area);
+
+// 8 
+
+// let p = 7;
+// let q = 22;
+// p += 1;
+// q -= 1;
+// console.log(p);
+// console.log(q);
+
+// 9 
+
+// let num1 = 23;
+// let num2 = 34;
+
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+// console.log(num1 !== num2);
+// console.log(num1 != num2);
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+
+// 10 
+
+// let num1 = 10;
+// let num2 = "10";
+
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+
+// 11 
+
+// let temp = 30;
+
+// if (temp > 25) {
+//     console.log(true);
+// }
+
+// 12 
+
+// let score = 50;
+
+// if (score != 100) {
+//     console.log("score is less than 100");
+// }
+
+// 13
+
+// const myNumber = 25.05;
+
+// console.log(typeof(myNumber))
+
+// 14
+
+// let isRaining = false;
+
+// if (isRaining == true) {
+//     console.log("Take an umbrella!")
+// }
+
+// 15
+
+// let str = "Nata";
+// console.log(str.length);
+
+// 16 
+
+// let firstName = "Nata";
+// let lastName = "kvata";
+// let fullName = firstName + lastName
+// console.log(fullName);
+
+// 17 
+
+// 18 
+
+// let num = 12;
+
+// let isEven = num % 2 === 0;
+// let isGreaterThanTen = num > 10;
+
+// console.log("Number:", num);
+// console.log("Is Even:", isEven);
+// console.log("Is Greater Than 10:", isGreaterThanTen);
+
+// 19 
+
+// let password = "**************";
+// if (password.length >= 8) {
+//     console.log("Password is strong");
+// }
+
+// 20 
+
+
+
+
---
+++ level 57/homework/index.js
@@ -0,0 +1,165 @@
+
+// const greeting = "Hello world!";
+// console.log(greeting);
+
+// let counter = 0;
+// console.log(counter);
+// counter = 10;
+// console.log(counter);
+
+// const birthYear = 2009;
+// console.log(birthYear);
+
+// let x = 25;
+// let y = 5;
+// console.log(x + y);
+
+// const firstName = "Nata";
+// console.log(firstName);
+
+// let temp = 25;
+// console.log(temp);
+
+// const isAdult = true;
+// console.log(isAdult);
+
+// let age = 15;
+// age = 16;
+// console.log(age);
+
+// const PI = 3.14159;
+// console.log(PI);
+
+// let a = 5;
+// let b = 3;
+// console.log(a + b);
+
+// let x = 7;
+// let y = 9;
+// let result = x * y;
+// console.log(result);
+
+// let m = 75;
+// let n = 3;
+// console.log(m / n);
+
+// let result = 15 - 8;
+// console.log(result);
+
+
+// let remainder = 10 % 4;
+// console.log(remainder);
+
+// let num = 200;
+// num = 200 + 100;
+// console.log(num);
+
+// let result = 20 * 4 / 10;
+// console.log(result);
+
+// let height = 20;
+// let width = 10;
+// let area = height * width;
+// console.log(area);
+
+// console.log(6 ** 2);
+
+// console.log((100 - 50) / 2);
+
+// let x = 20;
+// let y = 15;
+// console.log(x > y);
+
+// let a = 10;
+// let b = "10";
+// console.log(a === b);
+
+// let age1 = 18;
+// let age2 = 20;
+// console.log(age1 < age2);
+
+// let num1 = 5;
+// let num2 = 10;
+// console.log(num1 <= num2);
+
+// let p = 7;
+// let q = 8;
+// console.log(p !== q);
+
+// let num1 = 10;
+// let num2 = "10";
+// console.log(num1 >= num2);
+
+// let apple = "apple";
+// let banana = "banana";
+// let areEqual = apple == banana;
+// console.log(areEqual);
+
+// let bool1 = true;
+// let bool2 = false;
+// console.log(bool1 !== bool2);
+
+// console.log(5 > 3);
+
+// let num1 = 8;
+// let num2 = 12;
+// console.log(num1 < num2);
+
+// let myNumber = 45;
+// console.log(typeof(myNumber));
+
+// let result = 100 / 4;
+// console.log(typeof(result));
+
+// let result = 5 + 7;
+// console.log(typeof(result));
+
+// let largeNumber = 1000000;
+// console.log(largeNumber);
+
+// let num1 = 20;
+// let num2 = 5;
+// console.log(num1 + num2);
+
+// let result = 50 - 30;
+// console.log(result);
+
+// console.log(6 * 8);
+
+// let totalAmount = 500;
+// console.log(totalAmount);
+
+// console.log(13 % 5);
+
+// let result = 9 / 3;
+// console.log(typeof(result));
+
+// let isSunny = true;
+// console.log(isSunny);
+
+// let isRaining = false;
+// let notRaining = !isRaining;
+// console.log(notRaining);
+
+// let hasDriverLicense = false;
+// let hasCar = false;
+// console.log(typeof(hasCar, hasDriverLicense));
+
+// let isOpen = false;
+// console.log(isOpen);
+
+// let result = 5 > 3;
+// console.log(typeof(result));
+
+// let isActive = true;
+// console.log(isActive);
+
+// console.log(true !== false);
+
+// let isCompleted = false;
+// console.log(isCompleted);
+
+// console.log(typeof(false));
+
+// let isLoggedIn = true;
+// console.log(isLoggedIn);

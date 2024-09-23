3473d5c45e4ef05abb946a6e5c61f91025098636
nkvantaliani <nataliakvantaliani2009@gmail.com>
2024-09-24 01:21:01 +0400

    commit

---
+++ level 59/classwork/index.js
@@ -0,0 +1,21 @@
+
+
+let p1 = document.getElementById("p1");
+p1.textContent = ("HIIIIII");
+p1.style.color="blue";
+p1.style.backgroundColor = "pink";
+p1.style.fontSize = "20px"
+
+let p2 = document.getElementById("p2");
+p2.textContent = ("Nata");
+p2.style.color="green";
+p2.style.backgroundColor = "red";
+p2.style.fontSize = "11px"
+
+let p3 = document.getElementById("p3");
+p3.textContent = ("kvata");
+p3.style.color="yellow";
+p3.style.backgroundColor = "green";
+p3.style.fontSize = "30px"
+
+
---
+++ level 59/homework/index.js
@@ -0,0 +1,11 @@
+let p1 = document.getElementById("p1");
+let p1text = p1.textContent = ("ahahahaha");
+alert(p1text);
+
+let style = document.getElementById("styledP");
+
+let div = document.getElementById("pdiv");
+div.style.display = "grid";
+div.style.width = "500px";
+div.style.width = "300px";
+div.style.backgroundColor = "pink";
